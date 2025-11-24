e87bbb3397f5f53c90e1ba322ccd856575216059
MdRimon29 <mdraisul.i.rimon@gmail.com>
2025-11-24 12:20:23 +0600

    updated:

--- file_io/rname.py
+++ file_io/rname.py
@@ -42,13 +42,26 @@
 #     print(f"{nm['name']} takes {nm['course']} course.")
 
 
+### csv.reader
+# import csv
+# instructors=[]
+# with open("file_io/names.csv", "r") as file:
+#     reader = csv.reader(file)
+#     for read in reader:
+#         instructors.append({"name":read[0], "course":read[1]})
+    
+    
+# for nm in sorted(instructors, key= (lambda student:student["name"])):
+#     print(f"{nm['name']} takes {nm['course']} course.")
 
+
+### dictreader
 import csv
 instructors=[]
 with open("file_io/names.csv", "r") as file:
-    reader = csv.reader(file)
+    reader = csv.DictReader(file)
     for read in reader:
-        instructors.append({"name":read[0], "course":read[1]})
+        instructors.append({"name":read["name"], "course":read["course"]})
     
     
 for nm in sorted(instructors, key= (lambda student:student["name"])):
