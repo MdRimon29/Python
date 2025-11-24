fe58aee5d5c4a484ff1db30d799eae168afbab15
MdRimon29 <mdraisul.i.rimon@gmail.com>
2025-11-24 09:56:29 +0600

    updated for csv file

--- file_io/rname.py
+++ file_io/rname.py
@@ -10,7 +10,21 @@
 # for line in lines:
 #     print("Hello, ", line.rstrip())
 
+
+
 ### More shorter version
-with open("file_io/names.txt", "r") as file:
-    for line in sorted(file):
-        print("My name is ", line.rstrip()) # if we want reverse order then simply just add reverse="True"
+# with open("file_io/names.txt", "r") as file:
+#     for line in sorted(file):
+#         print("My name is ", line.rstrip()) # if we want reverse order then simply just add reverse="True"
+        
+        
+### Working on CSV
+# with open("file_io/names.csv", "r") as file:
+#     for line in file:
+#         name, courses = line.rstrip().split(",")    #using , for seperated values in lines by comma
+#         print(f"{name} takes {courses} course.")
+
+with open("file_io/names.csv", "r") as file:
+    for line in file:
+        name, courses = line.rstrip().split(",")    #using , for seperated values in lines by comma
+        print(f"{name} takes {courses} course.")
